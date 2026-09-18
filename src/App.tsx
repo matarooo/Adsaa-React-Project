92d91549ce26171607e91b7e9f82c33e3dd775b5
matarooo <mohammedmatar988@gmail.com>
2026-09-18 18:32:28 +0300

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import MainLayout from "./Pages/MainLayout"
 import Home from "./Pages/Home"
 import Blog from "./Pages/Blog"
 import PostDetails from "./Pages/PostDetails"
-const routes =createBrowserRouter([
-  {path:'/' , element: <MainLayout/> , 
+const routes = createBrowserRouter([
+  {
+    path: '/', 
+    element: <MainLayout />, 
     children: [
-    {index:true , element: <Home/>},
-    {path:'/blog' , element: <Blog/>},
-    {path: 'blog/:slug', element: <PostDetails /> } 
-  ]}
-])
+      { index: true, element: <Home /> },
+      { path: 'blog', element: <Blog /> },         
+      { path: 'blog/:slug', element: <PostDetails /> } 
+    ]
+  }
+], {
+  basename: "/Adsaa-React-Project" 
+});
+
 function App() {
 
   return (
